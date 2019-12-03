3ddcb62519e4cde5d045fe1c2c46139a19ed2cf2
Tony Bogdanov <support@tonybogdanov.com>
2019-12-03 15:09:07 +0200

    feat: added FunctionExistsException

---
+++ src/Expectation/FunctionExistsExpectation.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright (c) Tony Bogdanov <tonybogdanov@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace DataTraveller\Expectation;
+
+use DataTraveller\Expectation\Exceptions\UnexpectedDataException;
+use DataTraveller\Path\Path;
+
+/**
+ * Class FunctionExistsExpectation
+ *
+ * @package DataTraveller\Expectation
+ * @author Tony Bogdanov <tonybogdanov@gmail.com>
+ */
+class FunctionExistsExpectation implements ExpectationInterface {
+
+    /**
+     * @return string
+     */
+    public function getType(): string {
+
+        return 'functionExists';
+
+    }
+
+    /**
+     * @param $data
+     * @param Path|null $path
+     *
+     * @return $this
+     * @throws UnexpectedDataException
+     */
+    public function expect( $data, Path $path = null ) {
+
+        if ( ! function_exists( $data ) ) {
+
+            throw new UnexpectedDataException( $data, $this->getType(), $path );
+
+        }
+
+        return $this;
+
+    }
+
+}
