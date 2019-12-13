d4a5525a5fa4dc8d8775fee2cb51fa8fc85a7567
Tony Bogdanov <tonybogdanov@gmail.com>
2019-12-13 09:15:30 +0200

    feat: added Lower/LowerThanOrEqual and Greater/GreaterThanOrEqual expectations

---
+++ src/Expectation/LowerThanExpectation.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright (c) Tony Bogdanov <tonybogdanov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DataTraveller\Expectation;
+
+use DataTraveller\Expectation\Exceptions\UnexpectedDataException;
+use DataTraveller\Path\Path;
+
+/**
+ * Class LowerThanExpectation
+ *
+ * @package DataTraveller\Expectation
+ * @author Tony Bogdanov <tonybogdanov@gmail.com>
+ */
+class LowerThanExpectation implements ExpectationInterface {
+
+    /**
+     * @var int|float
+     */
+    protected $value;
+
+    /**
+     * LowerThanExpectation constructor.
+     *
+     * @param float|int $value
+     */
+    public function __construct( $value ) {
+
+        $this->value = $value;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string {
+
+        return 'lt( ' . $this->value . ' )';
+
+    }
+
+    /**
+     * @param $data
+     * @param Path|null $path
+     *
+     * @return $this
+     * @throws UnexpectedDataException
+     */
+    public function expect( $data, Path $path = null ) {
+
+        if ( $data >= $this->value ) {
+
+            throw new UnexpectedDataException( $data, $this->getType(), $path );
+
+        }
+
+        return $this;
+
+    }
+
+}
